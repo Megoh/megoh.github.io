05df41b5a39e0f328633f5e721f0ee98442cbefb
Megoh <dominikwolowicz@gmail.com>
2018-04-14 10:59:44 +0200

    interactive black carousel controls with white background

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$(function() {
+$(function () {
+   "use strict";
    $('.carousel').carousel({
       pause: false
-   })
+   });
+})
+
+$(function () {
+   $(".carousel").mouseenter(function() {
+      $(".carousel-control-prev, .carousel-control-next").css("visibility", "visible");
+   });
+   $(".carousel").mouseleave(function() {
+      $(".carousel-control-prev, .carousel-control-next").css("visibility", "hidden");
+   });
 })
