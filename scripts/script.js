d50e8e1eb187810fc9f84e76951b24e9a48b482f
Megoh <dominikwolowicz@gmail.com>
2019-05-01 12:00:50 +0200

    added opacity transition to rows of projects

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,29 @@ $(function() {
     navMain.collapse("hide");
   });
 });
+
+function isInViewport(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+var items = document.querySelectorAll(".row");
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("resize", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
