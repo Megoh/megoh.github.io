9e07377c4a5a4374158d63a962788b5504929ff2
Megoh <dominikwolowicz@gmail.com>
2019-05-04 12:27:27 +0200

    added typewriter effect to the About Me section

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+var amountOfCSSInjectionsLeft = 1;
+
 $(function() {
   var navMain = $(".navbar-collapse");
 
@@ -27,6 +29,55 @@ function callbackFunc() {
     }
   }
 }
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 100 - Math.random() * 100;
+
+  if (this.txt === fullTxt) {
+    delta = this.period;
+    // inject css
+    if (amountOfCSSInjectionsLeft === 1) {
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML =
+        ".typewrite > .wrap { border-right: .15em solid #000; animation: blink-caret 0.75s step-end infinite; }";
+      document.body.appendChild(css);
+      amountOfCSSInjectionsLeft = 0;
+    }
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+};
 
 window.addEventListener("load", callbackFunc);
 window.addEventListener("resize", callbackFunc);
